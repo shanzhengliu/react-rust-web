b52e7b82d33adc6559da20fc889f0c1851073e17
Shanzheng Liu <liushanzheng960522@outlook.com>
2024-05-05 18:42:04 +1000

    update code

--- backend-rust/src/main.rs
+++ backend-rust/src/main.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 mod route;
 mod api;
 mod assets;
@@ -12,11 +14,11 @@ async fn main() {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     let app_run_port = std::env::var("APP_RUN_PORT").unwrap_or("8080".to_string());
     log::info!("starting HTTP server at http://localhost:{}", &app_run_port );
-    HttpServer::new(|| {
-        App::new()
-            .configure(config)
 
-    })
+    let app = ||{ App::new()
+        .configure(config)};
+
+    HttpServer::new(app)
         .bind(format!("0.0.0.0:{}",app_run_port)).expect(format!("Can not bind to port {}", app_run_port).as_str())
         .run().await.unwrap()
 
